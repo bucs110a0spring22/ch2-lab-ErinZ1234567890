2198ba7770819d93eda8b5add951a4f7ba84fd74
Erin Zhao <erinzhao8100@gmail.com>
2022-02-10 13:37:11 +0000

    completed lab

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import random
 
 #Part A
 weeks = 16
+print("variable weeks:", weeks, type(weeks))
 classes = 5
+print("variable classes:", classes, type(classes))
 tuition = 6000
+print(tuition, type(tuition))
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+classesPerWeek = 2
+print("variable classesPerWeek", classesPerWeek, type(classesPerWeek))
+costPerClass = cost_per_week / float(classesPerWeek)
+print("Cost per class:", costPerClass)
 
 #Part B
+myList = ["strong", 100, "big", 0.5, 'l']
+randList = random.choice(myList)
+print(randList)
